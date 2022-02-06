10f604adef82185d1a91ddbd0f1416c51a38cb0f
dskrypa <dskrypa@gmail.com>
2022-02-06 16:48:00 -0500

    added more tests for exceptions and groups

--- tests/test_error_handling.py
+++ tests/test_error_handling.py
@@ -9,7 +9,8 @@ from unittest.mock import Mock
 
 sys.path.append(Path(__file__).parents[1].joinpath('lib').as_posix())
 from command_parser.error_handling import ErrorHandler, extended_error_handler, _handle_os_error
-from command_parser.exceptions import CommandParserException
+from command_parser.exceptions import CommandParserException, ParserExit, ParamUsageError, InvalidChoice
+from command_parser.parameters import Flag
 
 
 class ErrorHandlingTest(TestCase):
@@ -70,6 +71,28 @@ class ErrorHandlingTest(TestCase):
     def test_oserror_other_ignored(self):
         with redirect_stdout(Mock()), self.assertRaises(OSError), extended_error_handler:
             raise OSError(21, 'test')
+
+
+class ExceptionTest(TestCase):
+    def test_exit_str(self):
+        self.assertIn('foo', str(ParserExit(message='foo')))
+        self.assertNotIn('foo', str(ParserExit()))
+
+    def test_exit_exit(self):
+        mock = Mock(write=Mock())
+        with redirect_stderr(mock), self.assertRaises(SystemExit):
+            ParserExit(message='test').exit()
+
+        self.assertTrue(mock.write.called)
+
+    def test_usage_error_str(self):
+        self.assertEqual('test', str(ParamUsageError(None, 'test')))  # noqa
+        param_str = str(ParamUsageError(Flag('--foo'), 'test'))
+        self.assertTrue(param_str.startswith('argument'))
+        self.assertTrue(param_str.endswith('test'))
+
+    def test_multiple_invalid(self):
+        self.assertIn("choices: 'a', 'b'", str(InvalidChoice(Flag('--foo'), ['a', 'b'], ['c', 'd'])))
 
 
 if __name__ == '__main__':
--- tests/test_groups.py
+++ tests/test_groups.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from unittest import TestCase, main
 
 sys.path.append(Path(__file__).parents[1].joinpath('lib').as_posix())
-from command_parser import ParameterGroup, Command, Flag, UsageError
+from command_parser import ParameterGroup, Command, Flag, UsageError, ParameterDefinitionError
 
 log = logging.getLogger(__name__)
 
@@ -117,6 +117,28 @@ class GroupTest(TestCase):
         foo = Foo.parse([])
         self.assertFalse(foo.bar)
         self.assertFalse(foo.baz)
+
+    def test_reject_double_mutual(self):
+        with self.assertRaises(ParameterDefinitionError):
+            ParameterGroup(mutually_dependent=True, mutually_exclusive=True)
+
+    def test_register_all(self):
+        class Foo(Command):
+            group = ParameterGroup()
+            foo = Flag()
+            bar = Flag()
+            group.register_all([foo, bar])
+
+        self.assertTrue(all(p.group == Foo.group for p in (Foo.foo, Foo.bar)))
+        self.assertEqual(2, len(list(Foo.group)))
+
+    def test_repr(self):
+        group = ParameterGroup('foo', mutually_exclusive=True)
+        self.assertIn('m.exclusive=True', repr(group))
+
+    def test_description(self):
+        group = ParameterGroup('foo', mutually_exclusive=True)
+        self.assertIn('exclusive', group.format_description())
 
 
 if __name__ == '__main__':
