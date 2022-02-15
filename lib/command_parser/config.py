978e9c0f313422df5cd12fd6ac6dc7c17c275fc2
dskrypa <dskrypa@gmail.com>
2022-02-15 08:52:25 -0500

    added comments describing config attrs

--- lib/command_parser/config.py
+++ lib/command_parser/config.py
@@ -13,5 +13,8 @@ __all__ = ['CommandConfig']
 
 @dataclass
 class CommandConfig:
+    #: Whether multiple action_flag methods are allowed to run if they are all specified
     multiple_action_flags: Bool = False
+
+    #: Whether action_flag methods are allowed to be combined with a positional Action method in a given CLI invocation
     action_after_action_flags: Bool = False
