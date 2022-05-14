49a27ea406cdf63ad4f83d0a4e352e622f0dee94
dskrypa <dskrypa@gmail.com>
2022-05-14 15:22:55 -0400

    fixed main test to use explicit argv

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,12 +15,12 @@ class MainTest(TestCase):
         class Foo(Command):
             main = Mock()
 
-        cmd_main()
+        cmd_main([])
         self.assertTrue(Foo.main.called)
 
     def test_main_raises_error_on_0_cmds(self):
         with self.assertRaisesRegex(RuntimeError, 'no commands were found'):
-            cmd_main()
+            cmd_main([])
 
     def test_main_raises_error_on_2_cmds(self):
         class Foo(Command):
@@ -30,7 +30,7 @@ class MainTest(TestCase):
             pass
 
         with self.assertRaisesRegex(RuntimeError, 'found 2 commands:'):
-            cmd_main()
+            cmd_main([])
 
     def test_main_ignores_sub_cmd(self):
         class Foo(Command):
@@ -40,7 +40,7 @@ class MainTest(TestCase):
         class Bar(Foo):
             pass
 
-        cmd_main()
+        cmd_main([])
         self.assertTrue(Foo.parse_and_run.called)
 
 
