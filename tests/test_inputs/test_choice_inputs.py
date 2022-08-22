e161c48d1622912428466195ce714c444d24dfb4
dskrypa <dskrypa@gmail.com>
2022-08-22 07:43:42 -0400

    split test_inputs unit test module into package

---
+++ tests/test_inputs/test_choice_inputs.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+
+from enum import Enum
+from unittest import main, TestCase
+
+from cli_command_parser import Command, Option
+from cli_command_parser.core import get_params
+from cli_command_parser.exceptions import UsageError
+from cli_command_parser.inputs.choices import Choices, ChoiceMap, EnumChoices
+from cli_command_parser.inputs.exceptions import InvalidChoiceError
+from cli_command_parser.testing import ParserTest
+
+
+class EnumExample(Enum):
+    FOO = '1'
+    Bar = '2'
+    baz = '3'
+
+
+class ChoiceInputTest(TestCase):
+    def test_invalid_choice_multi(self):
+        e = InvalidChoiceError(('y', 'z'), ('a', 'b', 'c'))
+        self.assertEqual("invalid choices: 'y', 'z' (choose from: 'a', 'b', 'c')", str(e))
+
+    def test_enum_replaced(self):
+        class Foo(Command):
+            bar = Option(type=EnumExample)
+
+        self.assertIsInstance(Foo.bar.type, EnumChoices)
+
+    def test_enum_with_choices(self):
+        class Foo(Command):
+            bar = Option(type=EnumExample, choices=(EnumExample.FOO, EnumExample.Bar))
+
+        self.assertIsInstance(Foo.bar.type, Choices)
+        self.assertIsInstance(Foo.bar.type.type, EnumChoices)
+        self.assertIs(Foo.bar.type.type.enum, EnumExample)
+
+    def test_enum_repr(self):
+        expected = '<EnumChoices[type=EnumExample, case_sensitive=False, choices=(FOO,Bar,baz)]>'
+        self.assertEqual(expected, repr(EnumChoices(EnumExample)))
+
+    def test_enum_help_text(self):
+        class Foo(Command):
+            bar = Option('-b', type=EnumExample)
+
+        foo = Foo()
+        foo.ctx._terminal_width = 199
+        with foo.ctx:
+            self.assertIn('{FOO|Bar|baz}', get_params(foo).formatter.format_help())
+
+    def test_enum_case_sensitive(self):
+        # fmt: off
+        val_exp_map = {
+            'FOO': EnumExample.FOO, 'Bar': EnumExample.Bar, 'baz': EnumExample.baz,
+            '1': EnumExample.FOO, '2': EnumExample.Bar, '3': EnumExample.baz,
+        }
+        # fmt: on
+        ec = EnumChoices(EnumExample, case_sensitive=True)
+        for value, expected in val_exp_map.items():
+            self.assertEqual(expected, ec(value))
+            self.assertIn(value, ec)
+
+        for val in ('bar', 'test', 'foo', 'BAZ', '0', '4'):
+            with self.subTest(val=val):
+                self.assertNotIn(val, ec)
+                with self.assertRaisesRegex(InvalidChoiceError, "choose from: 'FOO', 'Bar', 'baz'"):
+                    ec(val)
+
+    def test_enum_case_insensitive(self):
+        # fmt: off
+        val_exp_map = {
+            'foo': EnumExample.FOO, 'FOO': EnumExample.FOO, '1': EnumExample.FOO,
+            'bar': EnumExample.Bar, 'Bar': EnumExample.Bar, 'BAR': EnumExample.Bar, '2': EnumExample.Bar,
+            'baz': EnumExample.baz, 'BAZ': EnumExample.baz, '3': EnumExample.baz,
+        }
+        # fmt: on
+        ec = EnumChoices(EnumExample, case_sensitive=False)
+        for value, expected in val_exp_map.items():
+            self.assertEqual(expected, ec(value))
+            self.assertIn(value, ec)
+
+        for val in ('test', 'BAT', '0', '4'):
+            with self.subTest(val=val):
+                self.assertNotIn(val, ec)
+                with self.assertRaisesRegex(InvalidChoiceError, "choose from: 'FOO', 'Bar', 'baz'"):
+                    ec(val)
+
+    def test_choices_rejects_typed_insensitive(self):
+        with self.assertRaisesRegex(TypeError, 'Cannot combine case_sensitive=False'):
+            Choices((1, 2, 3), case_sensitive=False)
+
+    def test_choices_rejects_bad_enum_choices(self):
+        with self.assertRaisesRegex(TypeError, 'Invalid choices='):
+            Choices(EnumExample._member_map_, EnumChoices(EnumExample))
+
+    def test_choices_typed_repr(self):
+        choices = Choices((1, 2, 3), type=int)
+        self.assertEqual('<Choices[type=int, case_sensitive=True, choices=(1,2,3)]>', repr(choices))
+
+    def test_choices_ints(self):
+        choices = Choices((1, 2, 3), type=int)
+        for n in range(1, 4):
+            self.assertEqual(n, choices(str(n)))
+
+        for val in ('-1', '0', '4', '10', 'foo'):
+            with self.subTest(val=val):
+                with self.assertRaisesRegex(InvalidChoiceError, 'choose from: 1, 2, 3'):
+                    choices(val)
+
+    def test_choices_strs_sensitive(self):
+        choices = Choices(('FOO', 'Bar', 'baz'))
+        for val in ('FOO', 'Bar', 'baz'):
+            self.assertIn(val, choices)
+
+        for val in ('test', 'BAT', '0', '4', 'foo', 'bar', 'BAZ'):
+            self.assertNotIn(val, choices)
+
+    def test_choices_strs_insensitive(self):
+        choices = Choices(('FOO', 'Bar', 'baz'), case_sensitive=False)
+        for val in ('FOO', 'Bar', 'baz', 'foo', 'bar', 'BAZ'):
+            self.assertIn(val, choices)
+
+        for val in ('test', 'BAT', '0', '4'):
+            self.assertNotIn(val, choices)
+
+    def test_choice_map_sensitive(self):
+        val_exp_map = {'FOO': 1, 'Bar': 2, 'baz': 3}
+        cm = ChoiceMap({'FOO': 1, 'Bar': 2, 'baz': 3})
+        for value, expected in val_exp_map.items():
+            self.assertEqual(expected, cm(value))
+
+        for val in ('test', 'BAT', '0', '4', 'foo', 'bar', 'BAZ'):
+            self.assertNotIn(val, cm)
+
+    def test_choice_map_insensitive(self):
+        val_exp_map = {'FOO': 1, 'Bar': 2, 'baz': 3, 'foo': 1, 'bar': 2, 'BAZ': 3}
+        cm = ChoiceMap({'FOO': 1, 'Bar': 2, 'baz': 3}, case_sensitive=False)
+        for value, expected in val_exp_map.items():
+            self.assertEqual(expected, cm(value))
+
+        for val in ('test', 'BAT', '0', '4'):
+            self.assertNotIn(val, cm)
+
+
+class ParseInputTest(ParserTest):
+    def test_int_choices(self):
+        class Foo(Command):
+            bar: int = Option('-b', choices=(1, 2))
+            baz = Option('-B', type=int, choices=(1, 2))
+
+        success_cases = [
+            (['-b1', '-B=2'], {'bar': 1, 'baz': 2}),
+            (['-b2', '-B=1'], {'bar': 2, 'baz': 1}),
+            (['-B=1'], {'bar': None, 'baz': 1}),
+        ]
+        fail_cases = [['-ba'], ['-b0'], ['-b3'], ['-Ba'], ['-B0'], ['-B3'], ['-b', '-1'], ['-b', '']]
+        self.assert_parse_results_cases(Foo, success_cases)
+        self.assert_parse_fails_cases(Foo, fail_cases, UsageError)
+
+    def test_choices_with_nargs_plus(self):
+        class Foo(Command):
+            bar: int = Option('-b', choices=(1, 2), nargs='+')
+
+        success_cases = [
+            (['-b1'], {'bar': [1]}),
+            (['-b2'], {'bar': [2]}),
+            (['-b', '1', '1', '1'], {'bar': [1, 1, 1]}),
+            (['--bar', '2'], {'bar': [2]}),
+        ]
+        fail_cases = [['-ba'], ['-b0'], ['-b', '1', '3'], ['-b', '-1'], ['-b', '11']]
+        self.assert_parse_results_cases(Foo, success_cases)
+        self.assert_parse_fails_cases(Foo, fail_cases, UsageError)
+
+    def test_enum_type_validation(self):
+        class Foo(Command):
+            bar = Option('-b', type=EnumExample)
+
+        # fmt: off
+        val_exp_map = {
+            'foo': EnumExample.FOO, 'FOO': EnumExample.FOO, '1': EnumExample.FOO,
+            'bar': EnumExample.Bar, 'Bar': EnumExample.Bar, 'BAR': EnumExample.Bar, '2': EnumExample.Bar,
+            'baz': EnumExample.baz, 'BAZ': EnumExample.baz, '3': EnumExample.baz,
+        }
+        # fmt: on
+        success_cases = [(['-b', val], {'bar': exp}) for val, exp in val_exp_map.items()]
+        fail_cases = [['-b', val] for val in ('test', 'BAT', '0', '4')]
+        self.assert_parse_results_cases(Foo, success_cases)
+        self.assert_parse_fails_cases(Foo, fail_cases, UsageError)
+
+    def test_enum_with_choices(self):
+        class Foo(Command):
+            bar = Option('-b', type=EnumExample, choices=(EnumExample.FOO, EnumExample.Bar))
+
+        # fmt: off
+        val_exp_map = {
+            'foo': EnumExample.FOO, 'FOO': EnumExample.FOO, '1': EnumExample.FOO,
+            'bar': EnumExample.Bar, 'Bar': EnumExample.Bar, 'BAR': EnumExample.Bar, '2': EnumExample.Bar,
+        }
+        # fmt: on
+        success_cases = [(['-b', val], {'bar': exp}) for val, exp in val_exp_map.items()]
+        fail_cases = [['-b', val] for val in ('test', 'BAT', '0', '4', 'baz', 'BAZ', '3')]
+        self.assert_parse_results_cases(Foo, success_cases)
+        self.assert_parse_fails_cases(Foo, fail_cases, UsageError)
+
+
+if __name__ == '__main__':
+    # import logging
+    # logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    try:
+        main(verbosity=2, exit=False)
+    except KeyboardInterrupt:
+        print()
---
+++ tests/test_inputs/test_numeric_inputs.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+from unittest import main, TestCase
+
+from cli_command_parser import Command, Option
+from cli_command_parser.exceptions import UsageError
+from cli_command_parser.inputs import Range, NumRange
+from cli_command_parser.testing import ParserTest
+
+
+class NumericInputTest(TestCase):
+    def test_range_replaced(self):
+        class Foo(Command):
+            bar: int = Option(type=range(10))
+            baz: int = Option(choices=range(10))
+
+        self.assertIsInstance(Foo.bar.type, Range)  # noqa
+        self.assertIsInstance(Foo.baz.type, Range)  # noqa
+
+    def test_range_with_choices_rejected(self):
+        with self.assertRaises(ValueError):
+            Option(type=range(10), choices=(1, 2))
+        with self.assertRaises(ValueError):
+            Option(type=Range(range(10)), choices=(1, 2))
+
+    def test_range_no_snap(self):
+        for case in (range(10), range(9, -1, -1)):
+            with self.subTest(case=case):
+                rng = Range(case)
+                for n in range(10):
+                    self.assertEqual(n, rng(str(n)))
+
+                for val in ('-1', '10', '11', '1.1'):
+                    with self.assertRaises(ValueError):
+                        rng(val)
+
+    def test_range_str(self):
+        self.assertEqual('{0 <= N <= 9}', Range(range(10)).format_metavar())
+        self.assertEqual('{0 <= N <= 9}', Range(range(9, -1, -1)).format_metavar())
+        self.assertEqual('{0 <= N <= 8, step=2}', Range(range(0, 10, 2)).format_metavar())
+        self.assertEqual('{0 <= N <= 8, step=2}', Range(range(8, -1, -2)).format_metavar())
+
+    def test_range_snap(self):
+        for case in (range(10), range(9, -1, -1)):
+            with self.subTest(case=case):
+                rng = Range(case, snap=True)
+                for n in range(10):
+                    self.assertEqual(n, rng(str(n)))
+
+                self.assertEqual(0, rng('-1'))
+                self.assertEqual(0, rng('-10'))
+                self.assertEqual(9, rng('10'))
+                self.assertEqual(9, rng('20'))
+
+    def test_range_with_type(self):
+        rng = Range(range(10), type=lambda x: int(x))
+        for n in range(10):
+            self.assertEqual(n, rng(str(n)))
+
+    def test_num_range_str(self):
+        self.assertEqual('0 <= N < 10', NumRange(min=0, max=10)._range_str())
+        self.assertEqual('0 <= N', NumRange(min=0)._range_str())
+        self.assertEqual('N < 10', NumRange(max=10)._range_str())
+        self.assertEqual('0 <= N <= 10', NumRange(min=0, max=10, include_max=True)._range_str())
+        self.assertEqual('0 < N < 10', NumRange(min=0, max=10, include_min=False)._range_str())
+        self.assertEqual('0 < N <= 10', NumRange(min=0, max=10, include_min=False, include_max=True)._range_str())
+
+    def test_num_range_requires_min_max(self):
+        with self.assertRaisesRegex(ValueError, 'at least one of min and/or max values'):
+            NumRange()
+
+    def test_num_range_requires_min_lt_max(self):
+        with self.assertRaisesRegex(ValueError, 'min must be less than max'):
+            NumRange(min=10, max=0)
+
+    def test_num_range_snap_requires_strict_range(self):
+        with self.assertRaisesRegex(ValueError, 'snap would produce invalid values'):
+            NumRange(snap=True, min=1, max=2)
+
+    def test_snap_rejects_float(self):
+        with self.assertRaisesRegex(TypeError, 'Unable to snap to extrema with type=float'):
+            NumRange(snap=True, type=float, min=1)
+
+    def test_num_range_auto_type(self):
+        self.assertIs(int, NumRange(min=1, max=10).type)
+        self.assertIs(float, NumRange(min=1.5, max=10).type)
+        self.assertIs(float, NumRange(min=1.5, max=10.5).type)
+
+    def test_num_range_snap_incl_min(self):
+        rng = NumRange(snap=True, min=0, max=10)
+        for n in range(10):
+            self.assertEqual(n, rng(str(n)))
+
+        self.assertEqual(0, rng('-1'))
+        self.assertEqual(0, rng('-10'))
+        self.assertEqual(9, rng('10'))
+        self.assertEqual(9, rng('20'))
+
+    def test_num_range_snap_excl_min(self):
+        rng = NumRange(snap=True, min=-1, max=10, include_min=False)
+        for n in range(10):
+            self.assertEqual(n, rng(str(n)))
+
+        self.assertEqual(0, rng('-1'))
+        self.assertEqual(0, rng('-10'))
+        self.assertEqual(9, rng('10'))
+        self.assertEqual(9, rng('20'))
+
+    def test_num_range_snap_incl_max(self):
+        rng = NumRange(snap=True, min=0, max=9, include_max=True)
+        for n in range(10):
+            self.assertEqual(n, rng(str(n)))
+
+        self.assertEqual(0, rng('-1'))
+        self.assertEqual(0, rng('-10'))
+        self.assertEqual(9, rng('10'))
+        self.assertEqual(9, rng('20'))
+
+    def test_num_range_unbound_min(self):
+        rng = NumRange(max=10)
+        for val in ('10', '20', '100'):
+            with self.assertRaises(ValueError):
+                rng(val)
+
+        for val in (0, 1, 9, -1, -20, -100):
+            self.assertEqual(val, rng(str(val)))
+
+    def test_num_range_unbound_max(self):
+        rng = NumRange(min=0)
+        for val in ('-1', '-10', '-100'):
+            with self.assertRaises(ValueError):
+                rng(val)
+
+        for val in (0, 1, 10, 100):
+            self.assertEqual(val, rng(str(val)))
+
+
+class ParseInputTest(ParserTest):
+    def test_range_type_validation(self):
+        class Foo(Command):
+            bar = Option('-b', type=Range(range(10)))
+
+        success_cases = [
+            (['-b0'], {'bar': 0}),
+            (['-b1'], {'bar': 1}),
+            (['-b', '5'], {'bar': 5}),
+            (['--bar', '9'], {'bar': 9}),
+        ]
+        fail_cases = [['-ba'], ['-b', '-1'], ['-b', '11']]
+        self.assert_parse_results_cases(Foo, success_cases)
+        self.assert_parse_fails_cases(Foo, fail_cases, UsageError)
+
+
+if __name__ == '__main__':
+    # import logging
+    # logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    try:
+        main(verbosity=2, exit=False)
+    except KeyboardInterrupt:
+        print()
