e917866fa94ba66b7b4c84658630233aaac8a608
dskrypa <dskrypa@users.noreply.github.com>
2023-05-02 11:36:21 +0000

    deploy: 2e9e5bb1b26d02a1d3023043f2ab28dc8e852f7f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.04.30',
+    VERSION: '2023.05.02',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
