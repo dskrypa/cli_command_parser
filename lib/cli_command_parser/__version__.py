93cec6ea7f850742bcfeaf502096d7dd5a4c8d20
dskrypa <dskrypa@gmail.com>
2024-09-07 14:01:48 -0400

    updated version to 2024.09.07

--- lib/cli_command_parser/__version__.py
+++ lib/cli_command_parser/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'cli_command_parser'
 __description__ = 'CLI Command Parser'
 __url__ = 'https://github.com/dskrypa/cli_command_parser'
-__version__ = '2024.08.17'
+__version__ = '2024.09.07'
 __author__ = 'Doug Skrypa'
 __author_email__ = 'dskrypa@gmail.com'
 __license__ = 'Apache 2.0'
